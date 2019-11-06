92d842f616293fedb6aec7c3926f50c4bad7a31b
q969210177 <969210177@qq.com>
2019-11-06 16:23:31 +0800

    2019年11月06日16:23:27

--- c_learn_vue/src/views/global_component/navmenu/navmenu.js
+++ c_learn_vue/src/views/global_component/navmenu/navmenu.js
@@ -4,27 +4,42 @@ export default {
     return {
       navMenuData: [
         {
+          name: "首页",
+          path: "/home",
+          permission: true,
+          iconFont: "el-icon-location",
+          type: "firstLevel"
+        },
+        {
+          name: "文档",
+          path: "/home",
+          permission: true,
+          iconFont: "el-icon-location",
+          type: "firstLevel"
+        },
+        {
           name: "系统设置",
           path: "/sys",
-          permission: "true",
+          permission: true,
           iconFont: "el-icon-s-tools",
+          type: "secondary",
           itemMenu: [
             {
               name: "用户管理",
               path: "/user",
-              permission: "false",
+              permission: false,
               iconFont: "el-icon-user-solid"
             },
             {
               name: "数据管理",
               path: "/data",
-              permission: "true",
+              permission: true,
               iconFont: "el-icon-s-data"
             },
             {
               name: "商户管理",
               path: "/shop",
-              permission: "true",
+              permission: true,
               iconFont: "el-icon-s-shop"
             }
           ]
@@ -32,14 +47,17 @@ export default {
         {
           name: "看一看",
           path: "/look",
-          permission: "true",
-          iconFont: "iconfont icon-guanliyuan"
+          permission: true,
+          iconFont: "iconfont icon-guanliyuan",
+          type: "secondary",
+          itemMenu: [{ name: "知乎", path: "/zhihu", permission: true }]
         },
         {
           name: "图标",
           path: "/icon",
-          permission: "true",
-          iconFont: "el-icon-s-management"
+          permission: true,
+          iconFont: "el-icon-s-management",
+          type: "firstLevel"
         }
       ],
       activeIndex2: "1",
@@ -47,6 +65,14 @@ export default {
         true: true,
         false: false
       }
+      // test: [
+      //   { type: "a", path: "index1" },
+      //   { type: "b", path: "index2" },
+      //   { type: "c", path: "index3" },
+      //   { type: "b", path: "index4" },
+      //   { type: "b", path: "index5" },
+      //   { type: "c", path: "index6" }
+      // ]
     };
   },
   methods: {
--- c_learn_vue/src/views/global_component/navmenu/navmenu.vue
+++ c_learn_vue/src/views/global_component/navmenu/navmenu.vue
@@ -1,53 +1,29 @@
 <template>
-    <div class="navmenu">
+  <div class="navmenu">
     <el-row>11</el-row>
     <el-row>
-        <!-- :collapse="$store.state.menuCollBol":router="publicBol.true" :unique-opened="publicBol.true" -->
-        <el-col>
-        <el-menu
-        :unique-opened="publicBol.true"
-        default-active="2"
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b">
-      <!-- el-submenu 是第一级 子组件 -->
-      <el-menu-item index="/home">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>首页</span>
-        </template>
-      </el-menu-item>
-
-      <el-menu-item index="/wendang">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>文档</span>
-        </template>
-      </el-menu-item>
-
-      <el-submenu :index="v.path" v-for="(v,k) in navMenuData" :key="k">
-        <template slot="title">
-          <i :class="v.iconFont"></i>
-          <span>{{v.name}}</span>
-        </template>
-        <!-- el-menu-item-group 是二级子组件 -->
-        <el-menu-item-group v-for="(item,index) in v.itemMenu" :key="index">
-            <template slot="title">{{item.name}}</template>
-            <el-menu-item  :v-if="item.permission" :index="item.path">
-                <i :class="item.iconFont"></i>
-              {{item.name}}
+      <!-- :collapse="$store.state.menuCollBol":router="publicBol.true" :unique-opened="publicBol.true" -->
+      <el-col>
+        <el-menu :unique-opened="publicBol.true" default-active="/home" background-color="#545c64"
+          active-text-color="#ffd04b" v-for="(v,k) in navMenuData" :key="k">
+          <el-menu-item v-if="v.type==='firstLevel'" :index="v.path">
+            <i :class="v.iconFont"></i>
+            <span>{{v.name}}</span>
+          </el-menu-item>
+          <el-submenu :index="v.path" v-else>
+            <template slot="title">
+              <i :class="v.iconFont"></i>
+              <span>{{v.name}}</span>
+            </template>
+            <el-menu-item v-for="(item,index) in v.itemMenu" :key="index" :index="item.path">
+              <i :class="item.iconFont"></i>
+              <span>{{item.name}}</span>
             </el-menu-item>
-        </el-menu-item-group>
-        <!-- <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu> -->
-      </el-submenu>
-      </el-menu>
-        </el-col>
+          </el-submenu>
+        </el-menu>
+      </el-col>
     </el-row>
-       {{navMenuData}}
-    </div>
+  </div>
 </template>
 <script src="./navmenu.js"></script>
 <style lang="scss" scoped>
@@ -55,8 +31,5 @@
         background: #000;
         height: 100vh;
         overflow: hidden;
-    }
-    .menu_father{
-        width: 100%;
     }
 </style>
