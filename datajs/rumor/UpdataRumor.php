c203ced3ee66c6712376a457c0bfa3eda899aaef
chenxi <969210177@qq.com>
2022-06-06 16:53:49 +0800

    删除一些bug

--- datajs/rumor/UpdataRumor.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin:*");
-include "../core/mysql.func.php";
-error_reporting(E_ALL ^ E_NOTICE);
-header("content-type:text/html;charset=utf-8");
-$link = cc();
-
-?>
